843dfe6aef1405f92bbb226a4262716862e16f70
ansh <ansh.arora00010@gmail.com>
2024-10-10 10:07:16 -0700

    commit

---
+++ tny_script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Todd Nash
+   Date: 2024-10-08
+
+*/
+
+document.getElementById("dayNow").innerHTML = `m/d/y/ <br /> h:m:s`;
